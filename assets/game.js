4b6df769b6b4e5ccdecc3e0c72414ea9521fd0c8
dcaban <davidjcaban@gmail.com>
2017-08-17 18:36:55 -0400

    front end done back end not

---
+++ assets/game.js
@@ -0,0 +1,23 @@
+
+
+var counter = 0;
+var slurmNumber = [Math.floor(Math.random() * 12)+1];
+var targetNumber = [Math.floor(Math.random() * 102)+19];
+var food= {
+    
+}
+
+// displays the random number the user is supposed to reach
+$("#randoNum").text(targetNumber);
+
+// checks logic on slurm to make sure a number between 1-12 comes out
+console.log(slurmNumber);
+
+// assign the value to the slurm button and set it to display on the counter
+
+//assign the value to the fishy button and set it to display on the counter
+
+//assign the value to the bachelor button and set it to display on the counter
+
+//assign the value to the manwhich button and set it to display on the counter
+
